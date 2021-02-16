5eac694dbe073be07a1256dc9f8bd0a9fd2071ec
NIKOLA <vidovic94@hotmail.com>
2021-02-17 00:27:40 +0100

    fix artice live admin

--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -41,9 +41,19 @@
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             @if ($article->article_category_id === 1)
-                                                <a href="/it-price/{{$article->id}}/{{$article->slug}}" target="_blank" style="text-decoration: none;">LIVE</a>
+                                                <a href="/it-price/{{$article->id}}/{{$article->slug}}" target="_blank" class="no-underline flex">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 h-6 w-6 text-indigo-600">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                                                    </svg>
+                                                    <div class="ml-3 text-sm text-indigo-600">Live</div>
+                                                </a>
                                             @else
-                                                <a href="/it-dogadjaji/{{$article->id}}/{{$article->slug}}" target="_blank" style="text-decoration: none;">Live</a>
+                                                <a href="/it-dogadjaji/{{$article->id}}/{{$article->slug}}" target="_blank" class="no-underline flex">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 h-6 w-6 text-indigo-600">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                                                    </svg>
+                                                    <div class="ml-3 text-sm text-indigo-600">Live</div>
+                                                </a>
                                             @endif
                                         </td>
                                          <td class="px-6 py-4 whitespace-nowrap">
