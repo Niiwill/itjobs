6d3cad655dca1bcaf28bc6b322723e2c6d300e0d
NIKOLA <vidovic94@hotmail.com>
2021-02-17 02:11:18 +0100

    preload gif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,20 @@
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
-
-       
     </head>
+    <style type="text/css">
+        .se-pre-con {
+            position: fixed;
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background: url('{{asset('storage/img/preloader.gif')}}') center no-repeat #fff;
+        }
+    </style>
     <body class="font-sans antialiased">
+        <div class="se-pre-con"></div>
         <div class="min-h-screen bg-gray-100">
             @livewire('admin-nav')
 
@@ -43,5 +53,11 @@
         @livewireScripts
         
         @stack('scripts')
+        <script type='text/javascript'>
+            $(window).on('load',function() {
+                // Animate loader off screen
+                $(".se-pre-con").fadeOut("slow");;
+            });
+        </script>
     </body>
 </html>
