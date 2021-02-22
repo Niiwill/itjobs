2d68732b3a22b28feceee1e40c62bafc740b16e4
Niiwill <Niiwill94@gmail.com>
2021-02-22 22:41:55 +0100

    Update home.blade.php
    fix margin top

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -893,7 +893,7 @@
         <h2>IT vijesti i priče</h2>
         <div class="row mt-5">
         @foreach ($articles as $article)
-            <div class="col-12 col-md-4 mt-5">
+            <div class="col-12 col-md-4">
                 <div class="card">
                     <img class="card-img-top" src="{{asset('storage/'.$article->image_url)}}" alt="{{$article->title}} slika">
                     <div class="card-body pt-4 px-0 pb-0">
