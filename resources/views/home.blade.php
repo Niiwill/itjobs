76a3ae90a31bdb5f1b2b79e72dc539265d07c174
niiwill <niiwill94@gmail.com>
2021-04-02 15:04:24 +0200

    fix css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="icon" type="image/x-icon" href="{{asset('storage/img/favicon.png')}}" />
         <title>ITPoslovi.me | Svi IT poslovi u Crnoj Gori, kompanije i poslodavci na jednom mestu</title>
-        <meta name="description" content="IT Poslovi, konkursi i slobodna radna mesta! Poslodavci i firme u Crnoj Gori na jednom mestu. Oglasi za programiranje, dizajn, administraciju, web poslove i socijalne mreze i drugo.">
-
+        <meta name="description" content="IT Poslovi, konkursi i slobodna radna mesta! Poslodavci i firme u Crnoj Gori na jednom mestu. Programeri Crna Gora - Oglasi za programiranje, dizajn, administraciju, web poslove i drugo">
         <link rel="canonical" href="https://itposlovi.me"/>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
         <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-         <!-- Jquery -->
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
         <!-- Select2 -->
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-        <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet">
+        <!-- Navigation CSS -->
+        <link href="{{ asset('css/navigation.css?v=50') }}" rel="stylesheet">
+        <!-- Home CSS -->
+        <link href="{{ asset('css/home.css?v=60') }}" rel="stylesheet">
+        <!-- Footer CSS -->
+        <link href="{{ asset('css/footer.css?v=50') }}" rel="stylesheet">
+        
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TETF7GV131"></script>
         <script>
@@ -28,14 +29,7 @@
           gtag('js', new Date());
 
           gtag('config', 'G-TETF7GV131');
-        </script>
-        <!-- Navigation CSS -->
-        <link href="{{ asset('css/navigation.css?v=15') }}" rel="stylesheet">
-        <!-- Home CSS -->
-        <link href="{{ asset('css/home.css?v=17') }}" rel="stylesheet">
-        <!-- Footer CSS -->
-        <link href="{{ asset('css/footer.css') }}" rel="stylesheet">                                             
-
+        </script>                                             
     </head>
     <body>
         <!-- NAVIGATION -->
@@ -349,7 +343,7 @@
         </div>
 
         <!-- FOR COMPANIES - NEW JOB ACTION -->
-        <div class="container">
+        <div class="container d-none d-sm-block">
             <div id="add-job" class="p-5 my-5">
                 <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-briefcase-fill mr-3" viewBox="0 0 16 16">
                     <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z"/>
@@ -402,22 +396,21 @@
         <!-- FOOTER -->
         @include('footer')
 
+        <!-- JQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <!-- Select2 JS -->
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
         <script type="text/javascript">
-        $('.search_tags').select2({
-            width: '100%',
-            maximumSelectionLength:1,
-            placeholder:'Java',
-            minimumInputLength: 2,
-            language: {
-                inputTooShort: function () { return ''; }
-            }
-        });
+            $('.search_tags').select2({
+                width: '100%',
+                maximumSelectionLength:1,
+                placeholder:'Java',
+                minimumInputLength: 2,
+                language: {
+                    inputTooShort: function () { return ''; }
+                }
+            });
         </script>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
 
