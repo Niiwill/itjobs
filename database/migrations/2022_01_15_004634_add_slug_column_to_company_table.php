cf92ead514cfaf422850794e89da6bf1321523c5
unknown <niiwill94@gmail.com>
2022-01-15 01:53:08 +0100

    compnies profile

--- database/migrations/2022_01_15_004634_add_slug_column_to_company_table.php
+++ database/migrations/2022_01_15_004634_add_slug_column_to_company_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSlugColumnToCompaniesTable extends Migration
+class AddSlugColumnToCompanyTable extends Migration
 {
     /**
      * Run the migrations.
