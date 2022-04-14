5a58587e70025bbd3ee483cf88fc1a557ddb14fb
unknown <niiwill94@gmail.com>
2022-04-14 02:15:09 +0200

    job single redesign v.1

--- resources/views/admin/job/index.blade.php
+++ resources/views/admin/job/index.blade.php
@@ -1,9 +1,7 @@
 @push('scripts')
 <script>
 $(document).ready(function() {
-    table
-    .order( [ 3, 'asc' ] )
-    .draw();
+    $('#datatable').DataTable().order([ 3, 'desc' ]).draw();
 } );
 
 </script>
@@ -11,16 +9,6 @@ $(document).ready(function() {
 
 
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <a href="{{ route('admin.jobs.create') }}">
-            <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <svg class=" h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#fff">
-                    <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-                </svg>
-                Dodaj oglas
-            </button>
-        </a>
-    </x-slot> --}}
 
     <div class="conatiner-fluid content-inner mt-4 py-0">
         <div class="row">
